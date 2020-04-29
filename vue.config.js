1239d88e687e70020258f62d5662c1de42ad900c
liyi <liyi@sinux.com>
2020-04-29 17:51:51 +0800

    modi

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	outputDir: 'public', // 输出
-	publicPath: process.env.NODE_ENV === 'production' ? : '/[vue_pro_engineer]/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/[vue_pro_engineer]/' : '/'
 }
